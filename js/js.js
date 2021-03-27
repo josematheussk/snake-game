4c6c9abae56c75348febdcef47c285d113b89238
José Santos <josematheussk@gmail.com>
2021-03-27 00:42:52 -0300

    fifth commit

--- js/js.js
+++ js/js.js
@@ -6,25 +6,47 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-let direction = "right"
+let direction = "right";
+let comida = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha() {
+function criarCobra() {
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box)
     }
 }
 
+function criarComida() {
+    context.fillStyle = "red";
+    context.fillRect(comida.x, comida.y, box, box);
+}
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
 
 function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
-    criarCobrinha();
+    criarCobra();
+    criarComida();
 
     let snakeX = snake[0].x
     let snakeY = snake[0].y
@@ -34,7 +56,12 @@ function iniciarJogo() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != comida.x || snakeY != comida.y) {
+        snake.pop();
+    } else {
+        comida.x = Math.floor(Math.random() * 15 + 1) * box;
+        comida.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
